2b3bd0a27788686effe01c755379b795a03cc108
nittashiori <nitta@impv.co.jp>
2022-12-07 15:48:48 +0900

    Reactの設定追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [react()]
 });
