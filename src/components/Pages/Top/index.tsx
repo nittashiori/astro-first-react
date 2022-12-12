dd0d20710e7f1c46bd76498ad0f4f50b75348d3b
nittashiori <nitta@impv.co.jp>
2022-12-12 11:15:38 +0900

    Topコンポーネント追加と各所調整

---
+++ src/components/Pages/Top/index.tsx
@@ -0,0 +1,13 @@
+import MainVisual from '../../Atoms/MainVisual';
+import Nav from '../../Molecules/Nav';
+
+const Top: React.FC = () => {
+  return (
+    <div>
+      <MainVisual />
+      <Nav />
+    </div>
+  )
+}
+
+export default Top;
