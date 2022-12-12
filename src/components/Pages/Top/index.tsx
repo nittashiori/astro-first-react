b909267f6af2637b20a0ae4ce4bec0c6cad892c0
nittashiori <nitta@impv.co.jp>
2022-12-12 14:32:33 +0900

    SectionBlockコンポーネント追加

--- src/components/Pages/Top/index.tsx
+++ src/components/Pages/Top/index.tsx
@@ -1,11 +1,25 @@
+import Styles from './styles.module.css';
+
 import MainVisual from '../../Atoms/MainVisual';
 import Nav from '../../Molecules/Nav';
+import SectionBlock from '../../Organaisms/SectionBlock';
 
 const Top: React.FC = () => {
   return (
     <div>
       <MainVisual />
       <Nav />
+      <div className={Styles.contents}>
+        <SectionBlock
+          image="/images/concept.jpg"
+          imageWebp="/images/concept.webp"
+          titleEn="Concept"
+          titleJp="心静かにコーヒーを"
+          text="立ち止まって、
+          空の青さを眺めて、
+          一息つきましょう"
+        />
+      </div>
     </div>
   )
 }
