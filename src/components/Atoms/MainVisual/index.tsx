78724e6686deea771e16666b66a2b937a47f7ae8
nittashiori <nitta@impv.co.jp>
2022-12-13 10:33:05 +0900

    画像の指定修正

--- src/components/Atoms/MainVisual/index.tsx
+++ src/components/Atoms/MainVisual/index.tsx
@@ -6,8 +6,8 @@ const MainVisual: React.FC = () => {
       <picture>
         <source media="(max-width: 767px)" type="image/webp" srcSet="/images/sp_main-visual.webp" />
         <source media="(max-width: 767px)" srcSet="/images/sp_main-visual.jpg" />
-        <source type="image/webp" src="/images/pc_main-visual@2x.webp" srcSet="/images/pc_main-visual@2x.webp 1x, /images/pc_main-visual@2x.webp 2x" />
-        <img src="/images/pc_main-visual.jpg" srcSet="/images/pc_main-visual@2x.webp 1x, /images/pc_main-visual@2x.jpg 2x" alt="" />
+        <source type="image/webp" src="/images/pc_main-visual.webp" srcSet="/images/pc_main-visual@2x.webp 1x, /images/pc_main-visual@2x.webp 2x" />
+        <img src="/images/pc_main-visual.jpg" srcSet="/images/pc_main-visual.jpg 1x, /images/pc_main-visual@2x.jpg 2x" alt="" />
       </picture>
     </div>
   )
