9992a5661285658d0bbdfd536f6f0876be514e0c
nittashiori <nitta@impv.co.jp>
2022-12-13 10:45:03 +0900

    blogItemコンポーネント追加

---
+++ src/components/Molecules/BlogItem/index.tsx
@@ -0,0 +1,32 @@
+import Styles from './styles.module.css';
+
+type Props = {
+  image: string;
+  image2x: string;
+  imageWebp: string;
+  imageWebp2x: string;
+  title: string;
+  data: string;
+}
+
+const BlogItem: React.FC<Props> = ({ image, image2x, imageWebp, imageWebp2x, title, data }) => {
+  const srcSet = `${image} 1x, ${image2x} 2x`;
+  const webpSrcSet = `${imageWebp} 1x, ${imageWebp2x} 2x`;
+
+  return (
+    <a className={Styles.blogItem}>
+      <div className={Styles.eyecatch}>
+        <picture>
+          <source type="image/webp" src={imageWebp} srcSet={webpSrcSet} />
+          <img src={image} srcSet={srcSet} alt="" />
+        </picture>
+      </div>
+      <div className={Styles.body}>
+        <p className={Styles.title}>{title}</p>
+        <p className={Styles.data}>{data}</p>
+      </div>
+    </a>
+  )
+}
+
+export default BlogItem;
--- src/components/Pages/Top/index.tsx
+++ src/components/Pages/Top/index.tsx
@@ -4,6 +4,7 @@ import MainVisual from '../../Atoms/MainVisual';
 import Nav from '../../Molecules/Nav';
 import SectionBlock from '../../Organaisms/SectionBlock';
 import SectionTitle from '../../Atoms/SectionTitle';
+import BlogItem from '../../Molecules/BlogItem';
 
 const Top: React.FC = () => {
   return (
@@ -24,6 +25,14 @@ const Top: React.FC = () => {
           <SectionTitle
             titleEn="blog"
             titleJp="ブログ"
+          />
+          <BlogItem
+            image="/images/thumbnail01.jpg"
+            image2x="/images/thumbnail01@2x.jpg"
+            imageWebp="/images/thumbnail01.webp"
+            imageWebp2x="/images/thumbnail01@2x.webp"
+            title="コーヒーカー始めました。"
+            data="2030.08.08"
           />
         </section>
       </div>
