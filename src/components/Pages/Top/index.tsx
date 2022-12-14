4778d54d56dd26c352ea19524f6da92d849f9277
nittashiori <nitta@impv.co.jp>
2022-12-14 16:28:16 +0900

    SpNavコンポーネント追加

--- src/components/Pages/Top/index.tsx
+++ src/components/Pages/Top/index.tsx
@@ -1,5 +1,6 @@
 import Styles from './styles.module.css';
 
+import SpNav from '../../Molecules/Nav';
 import MainVisual from '../../Atoms/MainVisual';
 import Nav from '../../Molecules/Nav';
 import Concept from '../../Organaisms/Concept';
@@ -8,6 +9,7 @@ import Blog from '../../Organaisms/Blog';
 const Top: React.FC = () => {
   return (
     <div>
+      <SpNav />
       <MainVisual />
       <Nav />
       <div className={Styles.contents}>
