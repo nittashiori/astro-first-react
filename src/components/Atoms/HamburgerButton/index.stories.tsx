3fff5ac3233ea97c61c4b6bb363cd378cc5e5164
nittashiori <nitta@impv.co.jp>
2022-12-15 10:14:57 +0900

    HamburgerButtonコンポーネント追加

---
+++ src/components/Atoms/HamburgerButton/index.stories.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+import HamburgerButton from './index';
+
+export default {
+  title: 'Atoms/HamburgerButton',
+  component: HamburgerButton,
+};
+
+export const $default: React.FC = () => {
+  return (
+    <HamburgerButton />
+  )
+}
---
+++ src/components/Atoms/HamburgerButton/index.tsx
@@ -0,0 +1,23 @@
+import Styles from './styles.module.css';
+
+import { useState } from 'react';
+
+  const HamburgerButton: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen(!open);
+
+  return (
+    <button
+      className={[Styles.HamburgerButton, open ? Styles.isActive : ''].join(' ')}
+      onClick={() => toggle()}
+    >
+      <span className={Styles.inner}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </button>
+  )
+}
+
+export default HamburgerButton;
