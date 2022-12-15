eccfb942c172a747302f1b6e2bc9fc70c020ac00
nittashiori <nitta@impv.co.jp>
2022-12-15 10:51:56 +0900

    falseに戻してclient:visible追加

--- src/components/Atoms/HamburgerButton/index.tsx
+++ src/components/Atoms/HamburgerButton/index.tsx
@@ -3,7 +3,7 @@ import Styles from './styles.module.css';
 import { useState } from 'react';
 
   const HamburgerButton: React.FC = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   // const toggle = () => setOpen(!open);
 
   return (
