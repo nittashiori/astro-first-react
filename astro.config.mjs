515cf37fe0999811e46f72d19a34a0653b1700da
nittashiori <nitta@impv.co.jp>
2022-12-15 11:16:34 +0900

    astroアイランドアーキテクチャのサンプル追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react"
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react()]
+  integrations: [react(), preact()]
 });
